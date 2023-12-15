5e5008e06e4f5c181a99aa76c27bb7a8e96cba23
alvarorichard <alvaro10048@protonmail.com>
2023-12-15 20:33:51 -0300

    refactor code

--- main.c
+++ main.c
@@ -2,29 +2,26 @@
 #include <stdlib.h>
 
 #include <editline/readline.h>
-#include <editline/history.h>
+
+// se vc usa arch linux, remova a linha abaixo
+
+//#include <editline/history.h>
 
 int main(int argc, char** argv) {
 
-  /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
 
-  /* In a never ending loop */
+  puts("ZennityLang 0.0.0.0.1");
+  puts("Pressione Ctrl+c para Sair\n");
+
   while (1) {
 
-    /* Output our prompt and get input */
-    char* input = readline("lispy> ");
+    char* input = readline("ZennityLang> ");
 
-    /* Add input to history */
     add_history(input);
 
-    /* Echo input back to user */
-    printf("No you're a %s\n", input);
+    printf("Não é você %s\n", input);
 
-    /* Free retrieved input */
 
-    /* please do not consider this code as just a test */
     free(input);
 
   }

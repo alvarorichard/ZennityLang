a4ca42a6e4bea4e9a27ec726b943e9928bf34a15
alvarorichard <alvaro10048@protonmail.com>
2023-12-15 21:08:11 -0300

    add library on inlude folder

---
+++ doge_code.c
@@ -0,0 +1,28 @@
+#include "mpc.h" 
+#include "doge_code.h"
+
+int doge(){
+
+mpc_parser_t* Adjective= mpc_or(4, 
+    mpc_sym("uau"), mpc_sym("muitos"),
+    mpc_sym("tão"),  mpc_sym("tal")
+);
+
+/* Construir um analisador 'Substantivo' para reconhecer coisas */
+mpc_parser_t* Noun = mpc_or(5,
+    mpc_sym("lisp"), mpc_sym("linguagem"),
+    mpc_sym("livro"), mpc_sym("construir"), 
+    mpc_sym("c")
+);
+  
+  mpc_parser_t* Phrase = mpc_and(2, mpcf_strfold, 
+    Adjective, Noun, free);
+    mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
+
+    mpc_delete(Doge);
+
+    return 0;
+
+
+
+}
---
+++ include/doge_code.h
@@ -0,0 +1,9 @@
+#ifndef DOGE_CODE_H
+#define DOGE_CODE_H
+
+#include "mpc.h"
+
+// Declaração da função doge()
+int doge();
+
+#endif // DOGE_PARSER_H

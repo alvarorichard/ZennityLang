fbc6771eac7bf00e30bcc99573b0eb96e706ae8d
alvarorichard <alvaro10048@protonmail.com>
2023-12-17 02:25:37 -0300

    fix parsing.h v4

--- include/parsing.h
+++ include/parsing.h
@@ -14,12 +14,11 @@ void add_history(char* unused);
 
 /* Declarar tipos de dados para os parsers */
 typedef struct mpc_parser_t mpc_parser_t;
-typedef struct mpc_err_t mpc_err_t;
+struct mpc_err_t; // Fix: Remove 'typedef' keyword
 
 /* Funções para os parsers */
 mpc_parser_t* mpc_new(const char* name);
-//int mpca_lang(int flags, const char* language, ...);
-mpc_err_t* mpca_lang(int flags, const char* language, ...);
+mpc_err_t* mpc_lang(int flags, const char* language, ...); // Fix: Renamed function
 int mpc_parse(const char* filename, const char* input, mpc_parser_t* p, mpc_result_t* r);
 void mpc_cleanup(int num, ...);
 
